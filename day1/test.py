10459d809c420459fbe2b2617d9e5417f839e663
chickenmoo <l01022211617@gmail.com>
2023-01-02 21:12:15 +0900

    파이썬 복습

--- day1/test.py
+++ day1/test.py
@@ -1,27 +1,31 @@
-'''
-print = 출력
-+ = 더하기
-- = 빼기
-* = 곱하기
-/ = 나누기
-** = 거듭제곱
-// = 나눈 몫
-% = 나눈 나머지
-Ctrl + / = 한줄 주석처리
-작은 따옴표 3개 = 여러줄 주석처리
-'''
+# print = 출력
+# + = 더하기
+# - = 빼기
+# * = 곱하기
+# / = 나누기
+# ** = 거듭제곱
+# // = 나눈 몫
+# % = 나눈 나머지
+# Ctrl + / = 한줄 주석처리
+# 작은 따옴표 3개 = 여러줄 주석처리
 
-print("파이썬 시작")
-apple=10
-orange=20
-print(orange+apple)
-print(orange-apple)
-print(orange*apple)
-print(orange/apple)
-print(1/3)
-print(2**2)
-print(3**3)
-print(orange//apple)
-print(10//3)
-print(10%3)
-print(orange%apple)
+# print("파이썬 시작")
+# apple=10
+# orange=20
+# print(orange+apple)
+# print(orange-apple)
+# print(orange*apple)
+# print(orange/apple)
+# print(1/3)
+# print(2**2)
+# print(3**3)
+# print(orange//apple)
+# print(10//3)
+# print(10%3)
+# print(orange%apple)
+
+print(5**2)
+print(2**5)
+print(2**10)
+print(100//3)
+print(100%3)
