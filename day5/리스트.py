4f3d6689ff6b105b282b1d4da7a698c77ed62b3a
chickenmoo <l01022211617@gmail.com>
2023-02-13 22:29:46 +0900

    list

---
+++ day5/리스트.py
@@ -0,0 +1,72 @@
+'''
+   리스트(list)
+
+ - 열거형 변수를 나타내는 자료구조 중 하나
+ - 대괄호를 이용해서 표현
+ - 각 요소는 쉼표로 구분
+ - 비어있는 리스트는 "[]" 로 표현
+ - 메소드를 가짐
+ <특징1>
+  맨 오른쪽에 요소를 추가할 떄 append 메소드 사용
+ <특징2>
+  맨 오른쪽 요소를 삭제할 떄 pop 메소드 사용
+ <특징3>
+  리스트 전체를 비울 때는 clear 메소드 사용
+ <특징4>
+  오름차순 정렬할 때 sort 메소드 사용
+ <특징5>
+  리스트를 거꾸로 정렬할 때 reverse 메소드 사용
+'''
+# a=1
+# b="리스트"
+# c=[1,2,3,4]
+# print(c)
+# d=["사과","바나나","귤"]
+# print(d)
+# e=[1,"apple",2,"banana"]
+# print(e)
+# f=[]
+# print(f)
+# f.append(100)
+# print(f)
+# f.append(50)
+# print(f)
+# f.append(1)
+# print(f)
+
+# f.pop()
+# print(f)
+# f.pop()
+# print(f)
+# f.pop()
+# print(f)
+
+# g=[100,50,1]
+# g.clear()
+# print(g)
+
+# h=[2,4,1,3]
+# h.sort()
+# print(h)
+# i=["orange","cherry","apple"]
+# i.sort()
+# print(i)
+
+# j=[2,4,1,3]
+# j.reverse()
+# print(j)
+
+# Q1.
+# j=[2,4,1,3]
+# j.sort()
+# j.reverse()
+# print(j)
+
+# Q2.
+# k=[]
+# for i in range(1,21):
+#   if i%3==0:
+#     k.append(i)
+#     k.sort()
+#     k.reverse()
+# print(k)
